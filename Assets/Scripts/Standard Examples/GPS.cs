c957da0e0af61563ed8c030557bb7708aa739c8c
vrcorpru <head@vrcorp.ru>
2018-09-18 12:28:53 +0700

    no message

---
+++ Assets/Scripts/Standard Examples/GPS.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour {
+
+    public static GPS Instance { get; set; }
+
+    void Start () {
+        Instance = this;
+        StartCoroutine(GPSLocationInput());
+    }
+
+    public string Status
+    {
+        get; private set;
+    }
+
+	public float Latitude
+    {
+        get; private set;
+    }
+
+    public float Longitude
+    {
+        get; private set;
+    }
+
+    public float Altitude
+    {
+        get; private set;
+    }
+
+    public float HorizontalAccuracy
+    {
+        get; private set;
+    }
+
+    public float Timestamp
+    {
+        get; private set;
+    }
+
+
+    IEnumerator GPSLocationInput ()
+    {
+        // Проверка пользовательского разрешения на получение информации о местоположении
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        // Стартс службы определения местоположения
+        Input.location.Start();
+        Status = "Подключение...";
+        
+        // Ожидание инициализации
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Если сервис не ответил в течение лимита времени
+        if (maxWait < 1)
+        {
+            Status = "Время подключения истекло";
+            yield break;
+        }
+
+        // Соединение оборвано
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Status = "Недоступна информация о местоположении устройства";
+            yield break;
+        }
+        else
+        {
+            // Получение данных о местоположении и запись в соответствующие переменные
+            Status = "Соединение установленно";
+            Latitude = Input.location.lastData.latitude;
+            Longitude = Input.location.lastData.longitude;
+            Altitude = Input.location.lastData.altitude;
+            HorizontalAccuracy = Input.location.lastData.horizontalAccuracy;
+            Timestamp = (float)Input.location.lastData.timestamp;
+        }
+
+        // Остановка сервиса
+        Input.location.Stop();
+    }
+}
---
+++ Assets/Scripts/Standard Examples/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public Text GPSText;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SetGPSCoordinates", 1, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void SetGPSCoordinates ()
+    {
+        GPSText.text = "";
+        GPSText.text += "\n";
+        GPSText.text += "Статус подключения: " + GPS.Instance.Status;
+        GPSText.text += "\n";
+        GPSText.text += "Широта: " + GPS.Instance.Latitude.ToString();
+        GPSText.text += "\n";
+        GPSText.text += "Долгота: " + GPS.Instance.Longitude.ToString();
+        GPSText.text += "\n";
+        GPSText.text += "Высота над уровнем моря: " + GPS.Instance.Altitude.ToString();
+    }
+}
